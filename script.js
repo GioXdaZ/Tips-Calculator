42f2e604957cb2710f9187b093601d630424aedf
GioXdaZ <gio.daz@gmail.com>
2022-08-08 14:52:04 -0400

    3° commit

--- script.js
+++ script.js
@@ -2,27 +2,36 @@
 
 const $ = (selector) => document.querySelector(selector);
 
+// inputs
 const bill = $(".input-bill");
 const person = $(".input-person");
 const customTipPorcent = $(".custom-input");
 
-function inputBill() {
-  bill.addEventListener("keyup", function (e) {
-    console.log(e.target.value);
-  });
-}
-inputBill();
+// Buttons
+const tip5 = $(".tip-5");
+const tip10 = $(".tip-10");
+const tip15 = $(".tip-15");
+const tip25 = $(".tip-25");
+const tip50 = $(".tip-50");
 
-function inputPerson() {
-  person.addEventListener("keyup", function (e) {
-    console.log(e.target.value);
-  });
-}
-inputPerson();
+//Results
 
-function inputCustomPorcent() {
-  customTipPorcent.addEventListener("keyup", function (e) {
-    console.log(e.target.value);
-  });
+const tipAmount = $(".result--0");
+const total = $(".result--1");
+
+tipAmount.textContent = "$" + (0).toFixed(2);
+total.textContent = "$" + (0).toFixed(2);
+
+function calculator() {
+  let num0 = bill.value;
+  let num1 = customTipPorcent.value / 100;
+  let num2 = person.value;
+
+  let miEcuacion = (num0 * num1) / num2;
+  tipAmount.textContent = miEcuacion;
+
+  let miEcuacion2 = num0 / num2 + miEcuacion;
+  total.textContent = miEcuacion2;
+
+  return { miEcuacion, miEcuacion2 };
 }
-inputCustomPorcent();
